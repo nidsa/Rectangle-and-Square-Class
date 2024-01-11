6bb7c4efd7976a62bf07cdf546e1bffdf07d02c7
Sokunnidsa-Pheng <147480064+nidsa@users.noreply.github.com>
2024-01-11 08:23:08 +0700

    Update Main.java

--- Rectangle and Square class/src/Main.java
+++ Rectangle and Square class/src/Main.java
@@ -4,13 +4,13 @@ public class Main {
         double l=25;
         double w=10;
         double side=15;
-        Rectangle r =new Rectangle(w,l);
-        System.out.println("Area of Rectangle:"+r.getArea());
-        System.out.println("Perimeter of Rectangle:"+r.getPerimeter());
+        Rectangle rectangle =new Rectangle(w,l);
+        System.out.println("Area of Rectangle:"+rectangle.getArea());
+        System.out.println("Perimeter of Rectangle:"+rectangle.getPerimeter());
 
-        Square s=new Square(side);
-        System.out.println("Area of Square:"+s.getArea());
-        System.out.println("Perimeter of Square:"+s.getPerimeter());
+        Square square=new Square(side);
+        System.out.println("Area of Square:"+square.getArea());
+        System.out.println("Perimeter of Square:"+square.getPerimeter());
 
     }
 }
